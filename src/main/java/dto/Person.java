2c887af28b76700a040982735ae29422188a1380
Vassileios Bouzas <vbouzas@eurobank.gr>
2025-07-18 11:00:46 +0300

    DTOs with Activator for DMN Import Java Classes

---
+++ src/main/java/dto/Person.java
@@ -0,0 +1,51 @@
+package dto;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+public class Person {
+
+    private String name;
+
+    // @JsonSerialize(using = LocalDateSerializer.class)
+    // @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private LocalDate dob;
+
+    private Boolean isValid;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public Boolean getIsValid() {
+        return isValid;
+    }
+    
+    public Boolean isIsValid() {
+        return isValid;
+    }
+
+    public void setIsValid(Boolean isValid) {
+        this.isValid = isValid;
+    }
+
+    @Override
+    public String toString() {
+        return "Person [name=" + name + ", dob=" + dob + ", isValid=" + isValid + "]";
+    }    
+
+}
