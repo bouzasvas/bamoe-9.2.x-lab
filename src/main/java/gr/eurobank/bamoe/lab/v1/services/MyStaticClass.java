a3d74b378250ec46c71cef5ba7d5b7e92dffda20
Vassileios Bouzas <vbouzas@eurobank.gr>
2025-07-18 13:43:05 +0300

    Script from static class sample

---
+++ src/main/java/gr/eurobank/bamoe/lab/v1/services/MyStaticClass.java
@@ -0,0 +1,9 @@
+package gr.eurobank.bamoe.lab.v1.services;
+
+public class MyStaticClass {
+    
+    public static void log(String message) {
+        System.out.println("MyStaticClass :: " + message);
+    }
+
+}
